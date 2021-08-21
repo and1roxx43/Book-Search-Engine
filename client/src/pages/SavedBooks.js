9fd1c667a8a3de6abf65dfe526f4e3b52730bca9
andrew quenette <and1roxx43@gmail.com>
2021-08-21 23:20:59 +1000

    second heroku deploy

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -13,9 +13,6 @@ const userData = data?.me || {};
 
 const [removeBook] = useMutation(REMOVE_BOOK);
 
-
-// console.log(userData.username);
-
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
     const token = Auth.loggedIn() ? Auth.getToken() : null;
