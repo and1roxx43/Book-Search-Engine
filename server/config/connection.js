221bac4e73f126e3bfa624d4417411d6ca43a96d
andrew quenette <and1roxx43@gmail.com>
2021-08-22 15:22:44 +1000

    Updated connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://and1roxx43:Mar12Nat10240568@cluster0.j5j1s.mongodb.net/googlebooks?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
